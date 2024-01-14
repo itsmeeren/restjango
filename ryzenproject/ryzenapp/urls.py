01cb3945c08afa46639a78e06476f15c48ab3218
karthik <karthik@kali.kali>
2024-01-14 11:16:03 +0530

    letmeknowguys

--- ryzenproject/ryzenapp/urls.py
+++ ryzenproject/ryzenapp/urls.py
@@ -5,7 +5,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path('', views.homepage,name='Home'),
-    path('Datas/', views.Datalist.as_view()),
+    path('Datas/<int:x>', views.name_detail),
 
 
 ]
--- ryzenproject/ryzenapp/views.py
+++ ryzenproject/ryzenapp/views.py
@@ -5,19 +5,26 @@ from rest_framework import status
 from rest_framework.response import Response
 from .models import Data
 from .serializers import DataSerializers
+
 from rest_framework.renderers import JSONRenderer
 # Create your views here.
 
-class Datalist(APIView):
-    def get(self,request):
-        Data1=Data.objects.all()
-        serializer=DataSerializers(Data1,many=True)
-        return Response(serializer.data)
-        # json_data=JSONRenderer().render(serializer.data)
-        # return Response(json_data)
-    # this above method is teh another rendering directly from the json renderer of rest
-    # which actually renders all the data in the single line and which can be seen in a single
-    # line
-    # for that i have to import the json renderer for that to work
+# class Datalist(APIView):
+#     def get(self,request):
+#         Data1=Data.objects.all()
+#         serializer=DataSerializers(Data1,many=True)
+#         #return Response(serializer.data)
+#         json_data=JSONRenderer().render(serializer.data)
+#         return HttpResponse(json_data,content_type='application/json')
+#     # this above method is teh another rendering directly from the json renderer of rest
+#     # which actually renders all the data in the single line and which can be seen in a single
+#     # line
+#     # for that i have to import the json renderer for that to work
+def name_detail(request,x):
+    Data1 = Data.objects.get(id=x)
+    serializer = DataSerializers(Data1)
+    # return Response(serializer.data)
+    json_data = JSONRenderer().render(serializer.data)
+    return HttpResponse(json_data, content_type='application/json')
 def homepage(request):
     return HttpResponse("Welcome , for datas  go to -> /Datas end point")
